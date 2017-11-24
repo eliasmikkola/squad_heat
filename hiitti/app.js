ec5bb1563656338beb11d4bad079bc4665b19ef5
Eepu <eemeli.vanhanen@gmail.com>
2017-11-24 21:58:52 +0200

    Paper js is lit

--- hiitti/app.js
+++ hiitti/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var paperjs = require('paper');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
